2b44bf23227ef93feef3635e524a2eb65d79f6ec
lihaitao <lihaitao_jkcpx@kedacom.com>
2019-09-24 18:02:12 +0800

    提交并发模块笔记9/24

---
+++ src/main/java/hliu/demo/highConcurrency/Counter.java
@@ -0,0 +1,67 @@
+package hliu.demo.highConcurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author LiHaitao
+ * @description Counter:Cas实现原子操作
+ * @date 2019/9/24 16:40
+ **/
+public class Counter {
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    private int i = 0;
+
+    public static void main(String[] args) {
+        final Counter cas = new Counter();
+        List<Thread> ts = new ArrayList<Thread>(600);
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 100; j++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        cas.count();
+                        cas.safeCount();
+                    }
+                }
+            });
+            ts.add(t);
+        }
+        for (Thread t : ts) {
+            t.start();
+        }
+        //等待所有线程执行完成
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(cas.i);
+        System.out.println(cas.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    /**
+     * 使用cas实现线程安全计数器
+     */
+    private void safeCount() {
+        for (; ; ) {
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 非线程安全计数器
+     */
+    private void count() {
+        i++;
+    }
+}
